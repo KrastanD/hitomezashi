7d14157f2d9fcc579bf77e22fb1fca404abad7a9
Krastan Dimitrov <krastan.kdd@gmail.com>
2022-07-03 07:17:02 -0500

    Split out vertical options

--- index.ts
+++ index.ts
@@ -1,171 +1,11 @@
 import { drawPattern } from "./pattern";
-import { DrawPatternProps, Sequence, Stroke } from "./types";
-import { isColor } from "./utils";
 import "./sidebar";
 import "./horizontal";
+import "./vertical";
 
-let ctx: CanvasRenderingContext2D,
-  canvas: HTMLCanvasElement,
-  sidebarVerticalSeqInput: HTMLInputElement,
-  sidebarVerticalSeqSelect: HTMLSelectElement,
-  verticalSequenceType: Sequence,
-  verticalCheckbox: HTMLInputElement,
-  verticalStrokeSelect: HTMLSelectElement,
-  verticalStrokeInput: HTMLInputElement,
-  verticalStrokeType: Stroke;
+resizeCanvas();
 
-const sidebar = document.getElementById("mySidebar") as HTMLDivElement;
-
-(function () {
-  canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
-  ctx = canvas.getContext("2d");
-
-  sidebarVerticalSeqInput = document.forms[0][
-    "verticalSequence"
-  ] as HTMLInputElement;
-
-  sidebarVerticalSeqSelect = document.forms[0][
-    "verticalSelect"
-  ] as HTMLSelectElement;
-
-  verticalCheckbox = document.forms[0]["verticalCheckbox"] as HTMLInputElement;
-  verticalStrokeSelect = document.forms[0][
-    "verticalStrokeSelect"
-  ] as HTMLSelectElement;
-  verticalStrokeInput = document.forms[0][
-    "verticalStrokeInput"
-  ] as HTMLInputElement;
-
-  verticalSequenceType = Number(sidebarVerticalSeqSelect.value);
-  verticalStrokeType = Number(verticalStrokeSelect.value);
-
-  canvas.width =
-    (window.innerWidth ||
-      document.documentElement.clientWidth ||
-      document.body.clientWidth) - sidebar.clientWidth;
-  canvas.height =
-    window.innerHeight ||
-    document.documentElement.clientHeight ||
-    document.body.clientHeight;
-})();
-
-const defaultDrawPatternProps: DrawPatternProps = {
-  sequenceOptions: {
-    isSequenceVisible: true,
-    sequenceType: Sequence.Random,
-    sequence: "",
-  },
-  strokeOptions: { stroke: Stroke.Rainbow },
-};
-let horizontalProps = { ...defaultDrawPatternProps };
-let verticalProps = { ...defaultDrawPatternProps };
-
-drawPattern(horizontalProps, verticalProps);
-
-sidebarVerticalSeqInput.oninput = function (event) {
-  const { value } = event.target as HTMLInputElement;
-
-  switch (verticalSequenceType) {
-    case Sequence.Binary:
-      sidebarVerticalSeqInput.title = "1s and 0s only";
-      sidebarVerticalSeqInput.pattern = "^[01]*$";
-      break;
-    case Sequence.DecimalToBinary:
-    case Sequence.DecimalParity:
-      sidebarVerticalSeqInput.title = "0-9 only";
-      sidebarVerticalSeqInput.pattern = "^[0-9]*$";
-      break;
-    case Sequence.AlphabetParity:
-    case Sequence.AlphabetToBinary:
-      sidebarVerticalSeqInput.title = "Letters only";
-      sidebarVerticalSeqInput.pattern = "^[a-zA-Z]*$";
-      break;
-  }
-  if (sidebarVerticalSeqInput.reportValidity()) {
-    horizontalProps = {
-      ...horizontalProps,
-      sequenceOptions: {
-        isSequenceVisible: horizontalProps.sequenceOptions.isSequenceVisible,
-        sequence: value ? value : "1",
-        sequenceType: verticalSequenceType,
-      },
-    };
-    drawPattern(horizontalProps, verticalProps);
-  }
-};
-
-sidebarVerticalSeqSelect.oninput = function (event) {
-  const { value } = event.target as HTMLSelectElement;
-  const sequenceType = Number(value) as Sequence;
-  verticalSequenceType = sequenceType;
-  if (sequenceType === Sequence.Random) {
-    sidebarVerticalSeqInput.value = "";
-    sidebarVerticalSeqInput.style.display = "none";
-    horizontalProps = {
-      ...horizontalProps,
-      sequenceOptions: {
-        isSequenceVisible: horizontalProps.sequenceOptions.isSequenceVisible,
-        sequence: "",
-        sequenceType: Sequence.Random,
-      },
-    };
-    drawPattern(horizontalProps, verticalProps);
-  } else {
-    sidebarVerticalSeqInput.style.display = "block";
-  }
-  sidebarVerticalSeqInput.value = "";
-  drawPattern(horizontalProps, verticalProps);
-};
-
-verticalCheckbox.oninput = function (event) {
-  const { checked } = event.target as HTMLInputElement;
-  horizontalProps = {
-    ...horizontalProps,
-    sequenceOptions: {
-      isSequenceVisible: checked,
-      sequence: horizontalProps.sequenceOptions.sequence,
-      sequenceType: horizontalProps.sequenceOptions.sequenceType,
-    },
-  };
-  drawPattern(horizontalProps, verticalProps);
-};
-
-verticalStrokeInput.oninput = function (event) {
-  const { value } = event.target as HTMLInputElement;
-  if (isColor(value)) {
-    horizontalProps = {
-      ...horizontalProps,
-      strokeOptions: {
-        stroke: horizontalProps.strokeOptions.stroke,
-        color: value,
-      },
-    };
-    drawPattern(horizontalProps, verticalProps);
-  }
-};
-
-verticalStrokeSelect.oninput = function (event) {
-  const { value } = event.target as HTMLSelectElement;
-  const strokeType = Number(value) as Stroke;
-  verticalStrokeType = strokeType;
-  if (strokeType === Stroke.Random || strokeType === Stroke.Rainbow) {
-    verticalStrokeInput.value = "";
-    verticalStrokeInput.style.display = "none";
-  } else {
-    verticalStrokeInput.style.display = "block";
-    verticalStrokeInput.title = "HTML color or hex code";
-  }
-  verticalStrokeInput.value = "";
-  horizontalProps = {
-    ...horizontalProps,
-    strokeOptions: {
-      stroke: strokeType,
-    },
-  };
-  drawPattern(horizontalProps, verticalProps);
-};
-
-document.body.onresize = function resizeCanvas() {
+function resizeCanvas() {
   canvas.width =
     window.innerWidth ||
     document.documentElement.clientWidth ||
@@ -175,5 +15,7 @@ document.body.onresize = function resizeCanvas() {
     document.documentElement.clientHeight ||
     document.body.clientHeight;
 
-  drawPattern(horizontalProps, verticalProps);
-};
+  drawPattern();
+}
+
+document.body.onresize = resizeCanvas;
--- vertical.ts
+++ vertical.ts
@@ -2,23 +2,25 @@ import { drawPattern } from "./pattern";
 import { DrawPatternProps, Sequence, Stroke } from "./types";
 import { isColor } from "./utils";
 
-const sidebarHorizontalSeqInput = document.forms[0][
-  "horizontalSequence"
-] as HTMLInputElement;
-const sidebarHorizontalSeqSelect = document.forms[0][
-  "horizontalSelect"
-] as HTMLSelectElement;
-const horizontalCheckbox = document.forms[0][
-  "horizontalCheckbox"
-] as HTMLInputElement;
-const horizontalStrokeSelect = document.forms[0][
-  "horizontalStrokeSelect"
-] as HTMLSelectElement;
-const horizontalStrokeInput = document.forms[0][
-  "horizontalStrokeInput"
+const sidebarVerticalSeqInput = document.forms[0][
+  "verticalSequence"
 ] as HTMLInputElement;
 
-let horizontalSequenceType = Number(sidebarHorizontalSeqSelect.value);
+const sidebarVerticalSeqSelect = document.forms[0][
+  "verticalSelect"
+] as HTMLSelectElement;
+
+const verticalCheckbox = document.forms[0][
+  "verticalCheckbox"
+] as HTMLInputElement;
+const verticalStrokeSelect = document.forms[0][
+  "verticalStrokeSelect"
+] as HTMLSelectElement;
+const verticalStrokeInput = document.forms[0][
+  "verticalStrokeInput"
+] as HTMLInputElement;
+
+let verticalSequenceType = Number(sidebarVerticalSeqSelect.value);
 let drawProps: DrawPatternProps = {
   sequenceOptions: {
     isSequenceVisible: true,
@@ -28,44 +30,45 @@ let drawProps: DrawPatternProps = {
   strokeOptions: { stroke: Stroke.Rainbow },
 };
 
-sidebarHorizontalSeqInput.oninput = function (event) {
+sidebarVerticalSeqInput.oninput = function (event) {
   const { value } = event.target as HTMLInputElement;
-  switch (horizontalSequenceType) {
+
+  switch (verticalSequenceType) {
     case Sequence.Binary:
-      sidebarHorizontalSeqInput.title = "1s and 0s only";
-      sidebarHorizontalSeqInput.pattern = "^[01]*$";
+      sidebarVerticalSeqInput.title = "1s and 0s only";
+      sidebarVerticalSeqInput.pattern = "^[01]*$";
       break;
     case Sequence.DecimalToBinary:
     case Sequence.DecimalParity:
-      sidebarHorizontalSeqInput.title = "0-9 only";
-      sidebarHorizontalSeqInput.pattern = "^[0-9]*$";
+      sidebarVerticalSeqInput.title = "0-9 only";
+      sidebarVerticalSeqInput.pattern = "^[0-9]*$";
       break;
     case Sequence.AlphabetParity:
     case Sequence.AlphabetToBinary:
-      sidebarHorizontalSeqInput.title = "Letters only";
-      sidebarHorizontalSeqInput.pattern = "^[a-zA-Z]*$";
+      sidebarVerticalSeqInput.title = "Letters only";
+      sidebarVerticalSeqInput.pattern = "^[a-zA-Z]*$";
       break;
   }
-  if (sidebarHorizontalSeqInput.reportValidity()) {
+  if (sidebarVerticalSeqInput.reportValidity()) {
     drawProps = {
       ...drawProps,
       sequenceOptions: {
         isSequenceVisible: drawProps.sequenceOptions.isSequenceVisible,
         sequence: value ? value : "1",
-        sequenceType: horizontalSequenceType,
+        sequenceType: verticalSequenceType,
       },
     };
-    drawPattern(null, drawProps);
+    drawPattern(drawProps);
   }
 };
 
-sidebarHorizontalSeqSelect.oninput = function (event) {
+sidebarVerticalSeqSelect.oninput = function (event) {
   const { value } = event.target as HTMLSelectElement;
   const sequenceType = Number(value) as Sequence;
-  horizontalSequenceType = sequenceType;
+  verticalSequenceType = sequenceType;
   if (sequenceType === Sequence.Random) {
-    sidebarHorizontalSeqInput.value = "";
-    sidebarHorizontalSeqInput.style.display = "none";
+    sidebarVerticalSeqInput.value = "";
+    sidebarVerticalSeqInput.style.display = "none";
     drawProps = {
       ...drawProps,
       sequenceOptions: {
@@ -74,13 +77,15 @@ sidebarHorizontalSeqSelect.oninput = function (event) {
         sequenceType: Sequence.Random,
       },
     };
-    drawPattern(null, drawProps);
+    drawPattern(drawProps);
   } else {
-    sidebarHorizontalSeqInput.style.display = "block";
+    sidebarVerticalSeqInput.style.display = "block";
   }
+  sidebarVerticalSeqInput.value = "";
+  drawPattern(drawProps);
 };
 
-horizontalCheckbox.oninput = function (event) {
+verticalCheckbox.oninput = function (event) {
   const { checked } = event.target as HTMLInputElement;
   drawProps = {
     ...drawProps,
@@ -90,10 +95,10 @@ horizontalCheckbox.oninput = function (event) {
       sequenceType: drawProps.sequenceOptions.sequenceType,
     },
   };
-  drawPattern(null, drawProps);
+  drawPattern(drawProps);
 };
 
-horizontalStrokeInput.oninput = function (event) {
+verticalStrokeInput.oninput = function (event) {
   const { value } = event.target as HTMLInputElement;
   if (isColor(value)) {
     drawProps = {
@@ -103,26 +108,26 @@ horizontalStrokeInput.oninput = function (event) {
         color: value,
       },
     };
-    drawPattern(null, drawProps);
+    drawPattern(drawProps);
   }
 };
 
-horizontalStrokeSelect.oninput = function (event) {
+verticalStrokeSelect.oninput = function (event) {
   const { value } = event.target as HTMLSelectElement;
   const strokeType = Number(value) as Stroke;
   if (strokeType === Stroke.Random || strokeType === Stroke.Rainbow) {
-    horizontalStrokeInput.value = "";
-    horizontalStrokeInput.style.display = "none";
+    verticalStrokeInput.value = "";
+    verticalStrokeInput.style.display = "none";
   } else {
-    horizontalStrokeInput.style.display = "block";
-    horizontalStrokeInput.title = "HTML color or hex code";
+    verticalStrokeInput.style.display = "block";
+    verticalStrokeInput.title = "HTML color or hex code";
   }
-  horizontalStrokeInput.value = "";
+  verticalStrokeInput.value = "";
   drawProps = {
     ...drawProps,
     strokeOptions: {
       stroke: strokeType,
     },
   };
-  drawPattern(null, drawProps);
+  drawPattern(drawProps);
 };
