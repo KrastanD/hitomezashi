ffee080dc2b0d43f4e3146f5338947719872b1eb
Krastan Dimitrov <krastan.kdd@gmail.com>
2022-12-19 22:21:40 -0600

    Create class for a single directional pattern

---
+++ src/classPattern.ts
@@ -0,0 +1,46 @@
+import { Sequence, Stroke } from "./types";
+
+class Pattern {
+  private isSequenceVisible = false;
+  private sequenceType = Sequence.Random;
+  private sequence = "";
+  private stroke = Stroke.Random;
+  private strokeColor: undefined | string = undefined;
+  public getIsSequenceVisible() {
+    return this.isSequenceVisible;
+  }
+
+  public getSequenceType() {
+    return this.sequenceType;
+  }
+
+  public getSequence() {
+    return this.sequence;
+  }
+
+  public getStroke() {
+    return this.stroke;
+  }
+
+  public getStrokeColor() {
+    return this.strokeColor;
+  }
+
+  public setIsSequenceVisible(status: boolean) {
+    this.isSequenceVisible = status;
+  }
+
+  public setSequenceType(type: Sequence) {
+    this.sequenceType = type;
+  }
+
+  public setSequence(sequence: string) {
+    this.sequence = sequence;
+  }
+
+  public setStroke(stroke: Stroke) {
+    this.stroke = stroke;
+  }
+}
+
+export default Pattern;
